d944be9ae57e9dbec7e637b51e8632b8378ce3ff
Haydenvaughn <80510548+Haydenvaughn@users.noreply.github.com>
2025-08-13 23:31:28 -0700

    Create SOIL_GAM_ext_functions.r
    Original SOIL extnsion functions for GAM

---
+++ SOIL_GAM_ext_functions.r
@@ -0,0 +1,136 @@
+library(mboost)
+library(mgcv)
+library(tidyverse)
+library(MASS)
+
+# Boosting GAM and Extraction Function
+boost_extract = function(iters, y, X){
+  candidate_mat = matrix(0, nrow = iters,ncol = ncol(X))
+  data_comb = data.frame(y = y, X)
+  gam.fit = gamboost(y ~ ., 
+                     data = data_comb,
+                     baselearner = "bbs",
+                     control = boost_control(mstop = iters))
+  print(gam.fit$xselect())
+  for (i in seq_along(gam.fit$xselect())) {
+      candidate_mat[i:nrow(candidate_mat),gam.fit$xselect()[i]] = 1
+  }
+  return(candidate_mat)
+}
+
+# ARM weighting for GAM ext
+arm_gam <- function(candidate_mat, L, y, X) {
+  
+  # Storage for final weights
+  weights.mat <- matrix(0, nrow = L, ncol = nrow(candidate_mat))
+  
+  # Combine y and X into one data frame
+  data_comb <- data.frame(y = y, X)
+  #print(head(data_comb))
+  
+  #initial weights for candidate models
+  psi <- 1/nrow(candidate_mat)
+  
+  # Loop over repetitions
+  for (l in 1:L) {
+    
+    # Train/test split
+    train.index <- sample(1:nrow(data_comb), trunc(nrow(data_comb)/2))
+    test.index  <- setdiff(1:nrow(data_comb), train.index)
+    test.y      <- y[test.index]
+    # Initialize storage for this repetition
+    count.vec   <- rep(0, nrow(candidate_mat))
+    unique.row  <- rep(0, nrow(candidate_mat))
+    sig.hat     <- rep(NA, nrow(candidate_mat))
+    df.resid    <- rep(NA, nrow(candidate_mat))
+    fitted.vals <- matrix(NA, nrow = nrow(candidate_mat), ncol = length(train.index))
+    preds       <- matrix(NA, nrow = nrow(candidate_mat), ncol = length(test.index))
+    edf         <- rep(NA, nrow(candidate_mat))
+    C           <- rep(NA, nrow(candidate_mat))
+    num.weight  <- rep(NA, nrow(candidate_mat))
+    weight      <- rep(NA, nrow(candidate_mat))
+    # Loop over each candidate row
+    for (row_idx in 1:nrow(candidate_mat)) {
+      
+      # Check for duplicates among earlier rows
+      if (row_idx > 1) {
+        for (prev_idx in 1:(row_idx - 1)) {
+          if (all(candidate_mat[row_idx, ] == candidate_mat[prev_idx, ])) {
+            count.vec[row_idx]  <- count.vec[row_idx] + 1
+            unique.row[row_idx] <- prev_idx
+            break
+          }
+        }
+      }
+      
+      # If duplicate, reuse previous results
+      if (count.vec[row_idx] > 0) {
+        sig.hat[row_idx]     <- sig.hat[unique.row[row_idx]]
+        df.resid[row_idx]    <- df.resid[unique.row[row_idx]]
+        fitted.vals[row_idx, ] <- fitted.vals[unique.row[row_idx], ]
+        preds[row_idx, ]     <-  preds[unique.row[row_idx],]
+        edf[row_idx]         <-  edf[unique.row[row_idx]]
+        C[row_idx]           <-  C[unique.row[row_idx]]
+      } 
+      else {
+        # Extract the subset of variables for this candidate
+        selected_vars <- which(candidate_mat[row_idx, ] == 1)
+        data.sub  <- data_comb[, c(1, selected_vars + 1)] # +1 for y in col1
+    
+        
+        train.sub <- data.sub[train.index, ]
+        test.sub  <- data.sub[test.index, ]
+        #print(head(test.sub))
+        
+          
+        covariates <- setdiff(names(train.sub), "y")
+        smooth_terms <- paste0("s(", covariates, ', bs = "ps")')
+        formula_str  <- paste("y ~", paste(smooth_terms, collapse = " + "))
+          
+        gam_fit <- gam(as.formula(formula_str), data = train.sub)
+        
+        df.resid[row_idx]     <- gam_fit$df.residual 
+        fitted.vals[row_idx, ] <- gam_fit$fitted.values
+        sig.hat[row_idx] <-(sum((train.sub$y-fitted.vals[row_idx, ])^2))/
+                            df.resid[row_idx]
+        preds[row_idx,]  <- predict.gam(gam_fit, newdata = test.sub)
+        #print(preds[row_idx,])
+        edf[row_idx]     <- sum(gam_fit$edf)
+        #edf[row_idx]     <- length(train.index) - df.resid[row_idx]
+        #print(max(edf))
+        
+        
+      }
+      C[row_idx]       <- edf[row_idx] * log(exp(1)*max(edf,na.rm = TRUE)/edf[row_idx]) + 
+                            2 * log(edf[row_idx] + 2)
+      
+      num.weight[row_idx]  <- (exp(-psi * C[row_idx]) * 
+                          sig.hat[row_idx]^(-nrow(candidate_mat)) * 
+                          prod(exp( -sig.hat[row_idx]^(-2) * 
+                          (test.y - preds[row_idx,])^2 / 2 )))
+                          
+    }
+    
+    #print(df.resid)
+    #print(fitted.vals)
+    #print(sig.hat)
+    #print(preds)
+    #print(edf)
+    #print(C)
+    # calculate weights and set them in lth vector in weights mat
+    #print(preds)
+    #print(weight)
+    weights.mat[l, ] <- num.weight/sum(num.weight)
+  }
+  final_weights <- colMeans(weights.mat)
+  return(final_weights)
+}
+
+# SOIL_gam
+soil_gam = function(candidate_mat,arm_weights){
+  soil.vals = rep(NA, ncol(candidate_mat))
+  for (j in 1:ncol(candidate_mat)){
+    soil.vals[j] = sum(candidate_mat[,j]*arm_weights)
+  }
+  return(soil.vals)
+}
